051f4441f30ad29ed3efd183d484939057b9330c
OleksandraTsy <aleksashka0581@gmail.com>
2024-05-13 16:58:45 +0200

    create hw-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css'
+import Profile from "./components/Profile/Profile";
+import userData from "./components/Profile/userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./components/FriendList/friends.json"
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./components/TransactionHistory/transactions.json"
 
-const Paragraph = () => {
-  return <p></p>
-}
 
-function App() {
-  return (
-    <div>
-<Paragraph/>
-    </div>
-  )
-}
+const App = () => {
+    return (
+        <>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </>
+    );
+};
 
 export default App
